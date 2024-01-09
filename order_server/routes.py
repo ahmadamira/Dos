65e068e9bc4e0cbb9574668ce84b8d46ff321f78
amrsalman <103906208+amrsalman@users.noreply.github.com>
2024-01-10 00:30:38 +0200

    ordar lab2

--- order_server/routes.py
+++ order_server/routes.py
@@ -3,30 +3,48 @@ import requests
 
 CATALOG_ADDRESS = "http://flask_app_1:5000"  # Use the service name and port of the catalog server
 
+timeout = (0.15, 1.5)
+
 @app.route('/buy/<book_id>', methods=['PUT'])
 def buy(book_id):
     if not book_id.isnumeric():
         return {'message': 'Invalid book ID. Please provide a numeric ID.'}, 422
 
-    # Query the book from the catalog server
-    book_response = requests.get(f'{CATALOG_ADDRESS}/search/by_item/{book_id}')
+    while True:
+        try:
+            # Query the book from the catalog server
+            book_response = requests.get(f'{CATALOG_ADDRESS}/search/by_item/{book_id}')
 
-    if book_response.status_code == 404:
-        return {'message': 'Book not found.'}, 404
-    elif book_response.status_code != 200:
-        return {'message': 'Error querying the catalog server.'}, 500
+        except requests.RequestException:
+            return {'message': 'Could not connect to the catalog server'}, 504
+        
+        if book_response.status_code == 404:
+            return {'message': 'Book not found.'}, 404
+        
+        elif book_response.status_code != 200:
+            return book_response.content, book_response.status_code, book_response.headers.items()
 
         
 
-    book = book_response.json()
+        book = book_response.json()
 
-    if book['quantity'] <= 0:
-        return {'success': False, 'message': 'Book is out of stock.'}
+        if book['quantity'] <= 0:
+            return {'success': False, 'message': 'Book is out of stock.'}
 
-    # Update the book quantity on the catalog server
-    update_response = requests.put(f'{CATALOG_ADDRESS}/modify/{book_id}', json={'quantity': book['quantity'] - 1})
+        try:
+            # Update the book quantity on the catalog server
+            update_response = requests.put(f'{CATALOG_ADDRESS}/modify/{book_id}', json={'quantity': book['quantity'] - 1})
 
-    if update_response.status_code != 200:
-        return {'message': 'Error updating the catalog server.'}, 500
+        except requests.RequestException:
+            return {'message': 'Could not connect to the catalog server'}, 504
+
+        if update_response.status_code == 409:
+            continue
+
+        if update_response.status_code != 200:
+            return update_response.text, update_response.status_code, update_response.headers.items()
+        
+        else:
+            break
 
     return {'success': True, 'message': 'Book purchased successfully.'}
